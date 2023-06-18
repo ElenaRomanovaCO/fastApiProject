821a3898150a728f3c60512ac78af3d81cc70c2e
ElenaRomanovaCO <eromanova115@gmail.com>
2023-06-18 12:09:46 -0600

    added tests and requirements

---
+++ test_api_1.py
@@ -0,0 +1,18 @@
+import json
+import pytest
+from flask import Flask
+
+from application import application
+
+
+@pytest.fixture
+def client():
+    application.config['TESTING'] = True
+    with application.test_client() as client:
+        yield client
+
+
+def test_get_products(client):
+    response = client.get('/products')
+    assert response.status_code == 200
+    assert len(response.json) == 3  # Assuming 3 products are initially present
